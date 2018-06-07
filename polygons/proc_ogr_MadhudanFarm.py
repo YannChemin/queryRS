35ed6b9fe1f1f4f368a4e511a2a9de24d03420d3
Yann Chemin <ychemin@gmail.com>
2018-06-07 22:39:23 +0200

    Add stub for polygon extraction

---
+++ polygons/proc_ogr_MadhudanFarm.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+#import required libraries
+from osgeo import osr
+from osgeo import ogr
+
+#Part A: Convert points to polygon
+#---------------------------------
+#Define Feature corners
+#North
+point0=[73.043333,20.250019]
+#East
+point1=[73.043651,20.250000]
+#South
+point2=[73.043624,20.249771]
+#West
+point3=[73.043362,20.249741]
+
+#Define a ring entity
+ring0 = ogr.Geometry(ogr.wkbLinearRing)
+
+#Fill in the 4 points
+ring0.AddPoint(point0[0],point0[1])
+ring0.AddPoint(point1[0],point1[1])
+ring0.AddPoint(point2[0],point2[1])
+ring0.AddPoint(point3[0],point3[1])
+
+#make the polygon0
+polygon0 = ogr.Geometry(ogr.wkbPolygon)
+polygon0.AddGeometry(ring0)
+
+#Now the polygon0 entity is ready 
+#to be incorporated into the shapefile
+
+#PART B: Create Shapefile
+#------------------------
+#Import Shapefile Driver definitions
+driver = ogr.GetDriverByName('ESRI Shapefile')
+
+#Open the country shapefile
+shapef = driver.CreateDataSource('farm.shp')
+
+#Define WGS84 projection
+wgs = osr.SpatialReference()
+wgs.ImportFromEPSG(4326)
+
+#Query the first Layer (#0)
+layer0 = shapef.CreateLayer("farm", wgs, ogr.wkbPolygon)
+
+#PART C: Add the polygon to the shapefile
+#----------------------------------------
+#Get the Layer0 definition
+featureDefinition = layer0.GetLayerDefn()
+
+#Create feature definition
+feature0 = ogr.Feature(featureDefinition)
+
+#Set the polygon0 geometry
+feature0.SetGeometry(polygon0)
+
+#Add the feature to the shapefile
+layer0.CreateFeature(feature0)
+
+#Clean up the mess
+ring0.Destroy()
+polygon0.Destroy()
+feature0.Destroy()
+
+#close the shapefiles
+shapef.Destroy()
+
+#create shapefile projection file
+wgs.MorphToESRI()
+file = open("farm" + '.prj', 'w')
+file.write(wgs.ExportToWkt())
+file.close()
