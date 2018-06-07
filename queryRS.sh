041dd91d5e74a427f009ce31cf2c48006a9d75cb
Yann Chemin <ychemin@gmail.com>
2018-06-07 15:41:12 +0200

    update

--- queryRS.sh
+++ queryRS.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+# $1 : Source file (Raster image)
+# $2 : Longitude coordinate of the Raster image (North-South)
+# $3 : Latitude coordinate of the Raster image (East-West)
+
+#test for number output re='^-?[0-9]+$' for potential negative number
+re='^[0-9]+$'
+
+#Process search for pixel values
+val=$(gdallocationinfo -valonly -wgs84 $1 $2 $3)
+if [[ $val =~ re ]]; then echo $val; fi
