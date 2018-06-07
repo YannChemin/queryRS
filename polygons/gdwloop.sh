0728076aea354bc895cae749296bfc8db38a59e4
Yann Chemin <ychemin@gmail.com>
2018-06-08 00:09:33 +0200

    Added loop of subsetting with stats extraction

---
+++ polygons/gdwloop.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+nprocs=16
+cutline=farm
+VI=NDVI
+
+cd ~/RSDATA/OUT_L8_Ya
+
+#cutline work with GDALWARP (parallel job)
+for file in *$VI.tif
+do
+	gdalwarp -cutline $cutline.shp -crop_to_cutline $file $cutline\_$file -overwrite -q &
+done
+
+#This will wait until all subset are processed
+while kill -0 $(pidof gdw.sh) >/dev/null 2>&1
+do
+	sleep 1
+done
+
+#stats extraction (OpenMP code, very fast !)
+for file in $(ls $cutline\_*.tif)
+do
+	~/queryRS/trunk/polygons/stats $file 32768 >> $cutline.csv
+done
+
