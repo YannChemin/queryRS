fb89d6bf34d7517279d7431fbf8e4a9b066635c3
Yann Chemin <ychemin@gmail.com>
2018-06-08 21:42:29 +0200

    update

---
+++ getPixVal.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+#-------------------------------------------------
+#Original form
+#bash ./queryRSAll.sh "~/RSDATA/" $long $lat $vi
+#-------------------------------------------------
+# $long
+# $lat
+# $vi : either of these: NDVI NDWI LSWI NBR2
+#-------------------------------------------------
+# Output is caught by the python subprocess
+#-------------------------------------------------
+
+# Pythonized form 
+import sys
+import subprocess
+csvVals=subprocess.call(['~/remotesensing/trunk/queryRS/trunk/queryRSAll.sh',"~/RSDATA/",sys.argv[1],sys.argv[2],sys.argv[3]])
+print csvVals
+
